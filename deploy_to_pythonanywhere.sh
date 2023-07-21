964282f788532e68692e326a92d3482227b8313e
gmtommasini <gmtommasini@gmail.com>
2023-07-21 11:21:47 -0400

    printenv

--- deploy_to_pythonanywhere.sh
+++ deploy_to_pythonanywhere.sh
@@ -4,6 +4,11 @@ set -e
 
 # Install dependencies and trigger deployment via PythonAnywhere API
 pip install -r requirements.txt
+
+echo "--------------------------------------------------------------------------"
+printenv
+echo "--------------------------------------------------------------------------"
+
 curl "https://www.pythonanywhere.com/api/v0/user/${PYTHONANYWHERE_USERNAME}/webapps/${PYTHONANYWHERE_WEBAPP_DOMAIN}/reload/" \
   --request POST \
   --header "Authorization: Token ${PYTHONANYWHERE_API_TOKEN}"
