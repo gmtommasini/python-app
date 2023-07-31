a366c519db3986083ff5c141d0f7b65780436341
gmtommasini <gmtommasini@gmail.com>
2023-07-31 19:58:51 -0400

    oauth amd playlist flow working

--- src/music/api_music.py
+++ src/music/api_music.py
@@ -4,9 +4,10 @@ from flask import Blueprint, request, jsonify
 # Modules
 # from src.utils.dates import is_valid_date
 from .crawler import get_soup
-from .spotify import spot, get_token, get_oauth_url, generate_random_string
+from .spotify import get_token, get_oauth_url, generate_random_string, create_spotify_list, save_spotify_list, get_user_id_from_token
 from .track import Track
 from utils import dates
+import requests
 
 blueprint_api_music = Blueprint('music', __name__, url_prefix='/api/music')
 
@@ -24,7 +25,7 @@ def get_user_token():
         callback_code: str = request.args.get("callback_code")
         state: str = request.args.get("state")
         print("code,state: ", callback_code, state)
-        token = get_token(callback_code, )
+        token = get_token(callback_code)
         print("Token: ", token)
         resp = jsonify(userToken=token)
         print(resp)
@@ -33,30 +34,25 @@ def get_user_token():
         return jsonify(error)
 
 
-@blueprint_api_music.route('/auth', methods=['POST', 'GET'])
-def user_oauth_something():
-    """This is a test of Spotify initialization..."""
-    print("SOMETHING IS WORKING")
-    spot.initialize_instance()
-    return {"message": "OK"}, 200
-
-@blueprint_api_music.route('/callback')
-def spotify_callback():
-    """This is a test on the callback"""
-    code = request.args.get("code")  # Get the authorization code from the query parameters
-    print("CALLBACK code: ", code)
-    if code:
-        # Use the authorization code to get the access token
-        try:
-            print("going to call the handler")
-            user_id = spot.handle_callback(code)
-            print("USER_ID:", user_id)
-            return user_id
-        except Exception as e:
-            print(e)
-            return "ERROR on callback", 500
-    else:
-        return "Authorization failed. No code provided."
+@blueprint_api_music.route('/get_user_id', methods=['GET'])
+def get_user_id_poc_token():
+    print("GETTING USER ID")
+    if not (bearer_token := request.headers.get('Authorization')):
+        return "Authorization header missing", 400
+    return get_user_id_from_token(bearer_token)
+    # headers = {
+    #     'Authorization': f'Bearer {bearer_token}'
+    # }
+    # # Make a GET request to the /me endpoint
+    # response = requests.get('https://api.spotify.com/v1/me', headers=headers)
+    # print(response.json())
+    # if response.status_code == 200:
+    #     user_data = response.json()
+    #     user_id = user_data['id']
+    #     return user_id
+    # else:
+    #     print(f"Failed to fetch user data. Status code: {response.status_code}")
+    #     return None
 
 
 @blueprint_api_music.route('/', methods=['OPTIONS'], strict_slashes=False)
@@ -82,32 +78,47 @@ def get_week_songs_api():
 
 @blueprint_api_music.route('/', methods=['POST'])
 def create_list_api():
-    req = request.get_json()
-    # Validating request
-    if not req:
+    """
+        Example:
+        JSON request:
+        {
+            "list_of_songs": [
+                "Song Title 1",
+                "Song Title 2",
+                "Song Title 3"
+            ],
+            "number_of_songs": 5
+        }
+
+        Response:
+        (['spotify:track:4iV5W9uYEdYUVa79Axb7Rh', 'spotify:track:1301WleyT98MSxVHPZCA6M'], 200)
+    """
+
+    # Validating request: If a json body, and the 'list_of_songs' key was present
+    if not (req := request.get_json()):
         return "Json body not found", 400
-    list_of_songs = req.get('list_of_songs', None)
-    if not list_of_songs:
+    if not (list_of_songs := req.get('list_of_songs', None)):
         return "Key 'list_of_songs' not found", 400
     # Optional parameters
     number_of_songs = req.get('number_of_songs', 10)
     uris_list = create_spotify_list(list_of_songs, number_of_songs)
-    return uris_list, 200
+    return jsonify(uris_list), 200
 
 
 @blueprint_api_music.route('/save', methods=['POST'])
 def save_list_api():
     req = request.get_json()
-    # Validating request
+    # Validating request:  If a json body, the auth header, and the 'uri_list' key was present
     if not req:
         return "Json body not found", 400
-    uri_list = req.get('uri_list', None)
-    if not uri_list:
+    if not (bearer_token := request.headers.get('Authorization')):
+        return "Authorization header missing", 400
+    if not (uri_list := req.get('uri_list', None)):
         return "Key 'uri_list' not found", 400
     # Optional parameters
     list_name = req.get('list_name', None)
     date = req.get('date', None)
-    list_id = save_spotify_list(track_uris_list=uri_list, list_name=list_name, date=date)
+    list_id = save_spotify_list(bearer_token=bearer_token.split(' ')[1], track_uris_list=uri_list, list_name=list_name, date=date)
     return jsonify(list_id), 200
 
 
@@ -125,66 +136,35 @@ def get_week_songs(date: str) -> list:
     return artist_n_track
 
 
-def create_spotify_list(list_of_songs: list[(str, str)], number_of_songs: int = 10) -> list:
-    """
-    Creates a list of Spotify's songs URI to be saved to user account
-
-    Args:
-        list_of_songs: list of tuples (artist, track name)
-        number_of_songs (int): Number of songs in Spotify's Playlist
-    returns:
-        list of Spotify's tracks uris
-    """
-    print("create_spotify_list")
-    playlist = []
-    count = 0
-    if not spot.sp:
-        spot.initialize_instance()
-
-    for artist_n_track in list_of_songs:
-        try:
-            # track: Track = spot.search_song(name=artist_n_track[1], artist=artist_n_track[0], year=date.split("-")[0])
-            track: Track = spot.search_song(name=artist_n_track[1], artist=artist_n_track[0])
-        except Exception:
-            # traceback.print_exc()
-            pass
-        else:
-            if track:
-                playlist.append(track)
-                count += 1
-            else:
-                # print("TRACK NOT AVAILABLE FOR: ", artist_n_track)
-                pass
-        if count == number_of_songs:
-            break
-
-    print("Found on Spotify: ", len(playlist))
-    print("Not found on Spotify: ", len(spot.get_not_found()))
-
-    # return list of Spotify's tracks uris
-    return [track.get_track_uri() for track in playlist]
-    # list_id = spot.create_playlist(list_name=f"Top songs from week of {date}", tracks_uris=ids)
-
-
-def save_spotify_list(track_uris_list: list[str], date: str=None, list_name: str=None):
-    """
-    Saves/creates a Spotify playlist with the list of URIs
-
-    Args:
-        track_uris_list: list[str] : list of Spotify's tracks URIs
-        date: str : date of the list
-        list_name: str : Optional Playlist name on Spotify
-    returns:
-        Spotify's list ID
-    """
-    if not list_name:
-        if not date:
-            date = "some date :D"
-        list_name = f"Top songs from week of {date}"
-    return spot.create_playlist(list_name=list_name, tracks_uris=track_uris_list)
 
 
 
 
 
 
+
+
+# @blueprint_api_music.route('/auth', methods=['POST', 'GET'])
+# def user_oauth_something():
+#     """This is a test of Spotify initialization..."""
+#     print("SOMETHING IS WORKING")
+#     spot.initialize_instance()
+#     return {"message": "OK"}, 200
+
+# @blueprint_api_music.route('/callback')
+# def spotify_callback():
+#     """This is a test on the callback"""
+#     code = request.args.get("code")  # Get the authorization code from the query parameters
+#     print("CALLBACK code: ", code)
+#     if code:
+#         # Use the authorization code to get the access token
+#         try:
+#             print("going to call the handler")
+#             user_id = spot.handle_callback(code)
+#             print("USER_ID:", user_id)
+#             return user_id
+#         except Exception as e:
+#             print(e)
+#             return "ERROR on callback", 500
+#     else:
+#         return "Authorization failed. No code provided."
--- src/music/spotify.py
+++ src/music/spotify.py
@@ -4,7 +4,7 @@ This module is the engine to work with Spotify APIs
 
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-from .config import SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_TOKEN, SPOTIFY_REDIRECT_URL
+from .config import SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_TOKEN, SPOTIFY_REDIRECT_URL, SPOTIFY_API_HOST_URL
 from .track import Track
 
 import requests
@@ -18,10 +18,6 @@ import random
 # Also: https://developer.spotify.com/dashboard
 
 redirect_uri = SPOTIFY_REDIRECT_URL
-
-
-# redirect_uri = 'http://localhost:5000/api/music'
-# redirect_uri = 'http://localhost:5000/api/music/callback'
 
 
 def get_oauth_url() -> str:
@@ -59,24 +55,100 @@ def get_token(callback_code: str) -> str:
         return None
 
 
+def get_user_id_from_token(token: str) -> str:
+    print(f"get_user_id_from_token: {token=}")
+    headers = {
+        'Authorization': f'Bearer {token}'
+    }
+    # Make a GET request to the /me endpoint
+    # response = requests.get(f'{SPOTIFY_API_HOST_URL}/v1/me', headers=headers)
+    response = requests.get('https://api.spotify.com/v1/me', headers=headers)
+    print(response.json())
+    if response.status_code == 200:
+        user_data = response.json()
+        user_id = user_data['id']
+        return user_id
+    else:
+        print(f"Failed to fetch user data. Status code: {response.status_code}")
+        return None
+
+
+def create_spotify_list(list_of_songs: list[(str, str)], number_of_songs: int = 10) -> list:
+    """
+    Creates a list of Spotify's songs URI to be saved to user account
+
+    Args:
+        list_of_songs (list[(str,str)]): list of tuples (artist, track name)
+        number_of_songs (int): Number of songs in Spotify's Playlist
+    returns:
+        list of Spotify's tracks uris
+    """
+    print("create_spotify_list")
+    playlist: list[Track] = []
+    counter_of_songs_included = 0
+
+    for artist_n_track in list_of_songs:
+        try:
+            # track: Track = spot.search_song(name=artist_n_track[1], artist=artist_n_track[0], year=date.split("-")[0])
+            track: Track = spot.search_song(name=artist_n_track[1], artist=artist_n_track[0])
+        except Exception:
+            # traceback.print_exc()
+            pass
+        else:
+            if track:
+                playlist.append(track)
+                counter_of_songs_included += 1
+            else:
+                # print("TRACK NOT AVAILABLE FOR: ", artist_n_track)
+                pass
+        if counter_of_songs_included == number_of_songs:
+            break
+
+    print("Found on Spotify: ", len(playlist))
+    print("Not found on Spotify: ", len(spot.get_not_found()))
+
+    # return list of Spotify's tracks uris
+    return [track.get_track_uri() for track in playlist]
+    # list_id = spot.create_playlist(list_name=f"Top songs from week of {date}", tracks_uris=ids)
+
+
+def save_spotify_list(bearer_token: str, track_uris_list: list[str], date: str=None, list_name: str=None):
+    """
+    Saves/creates a Spotify playlist with the list of URIs
+
+    Args:
+        bearer_token (str) : User bearer token
+        track_uris_list: list[str] : list of Spotify's tracks URIs
+        date: str : date of the list
+        list_name: str : Optional Playlist name on Spotify
+    returns:
+        Spotify's list ID
+    """
+    if not (user_id := get_user_id_from_token(bearer_token)):
+        # TODO: Exception
+        raise Exception("No user id found")
+    print("USER ID: ", user_id)
+    if not list_name:
+        if not date:
+            date = "some date :D"
+        list_name = f"Top songs from week of {date}"
+
+    # return spot.create_playlist(user_id=user_id, list_name=list_name, tracks_uris=track_uris_list)
+    return spot.create_playlist(user_id="a5gsln4erqve1ulkh1tea0xxg", list_name=list_name, tracks_uris=track_uris_list)
+
+
 class SPTFY:
     """This class wraps the methods and attributes necessary to call Spotify's APIs"""
 
     def __init__(self) -> None:
         self.sp = None
-
-        self.user_id = None
         self.__tracks_not_found = []
 
     def initialize_instance(self) -> None:
-        print(redirect_uri)
-        # We want to initialize the oauth process just after the user decided to create a list
-
         self.sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIFY_CLIENT_ID,
                                                             client_secret=SPOTIFY_CLIENT_TOKEN,
                                                             redirect_uri=redirect_uri,
                                                             scope="playlist-modify-private,playlist-modify-public"))
-        self.user_id = self.sp.me()['id']  # user logged
 
     def get_not_found(self):
         return self.__tracks_not_found
@@ -97,22 +169,22 @@ class SPTFY:
 
         # Set the access token for the Spotify instance in the class
         self.sp = spotipy.Spotify(auth=access_token)
-        self.user_id = self.sp.me()['id']  # Update the user ID with the newly authenticated user
-
         return self.user_id
 
-    def create_playlist(self, list_name: str, tracks_uris: list[str] = None) -> str:
+    def create_playlist(self, user_id: str, list_name: str, tracks_uris: list[str] = None) -> str:
         """Creates a Playlist for the user and return the list ID"""
-        resp = self.sp.user_playlist_create(user=self.user_id, name=list_name)
-        list_id = resp['uri']
-        # print("List ID: ", list_id)
+        print(f"create_playlist - {user_id=} -  {list_name=}")
+        resp = self.sp.user_playlist_create(user=user_id, name=list_name)
+        print(f"create_playlist - line 178")
+        playlist_id = resp['uri']
+        print(f"create_playlist - {playlist_id=}")
         if tracks_uris:
-            self.add_tracks(list_id=list_id, tracks=tracks_uris)
-        return list_id
+            print(f"create_playlist - {tracks_uris=}")
+            self.add_tracks(user_id=user_id, list_id=playlist_id, tracks=tracks_uris)
+        return playlist_id
 
-    def add_tracks(self, list_id: str, tracks: list[str]):
-        self.sp.user_playlist_add_tracks(self.user_id, list_id, tracks)
-        # self.sp.playlist_add_items(self.user_id, list_id, tracks)
+    def add_tracks(self, user_id: str, list_id: str, tracks: list[str]):
+        self.sp.user_playlist_add_tracks(user_id, list_id, tracks)
 
     def search_song(self, name: str = None, artist: str = None, year: str = None) -> Track:
         # print(name, artist, year)
@@ -143,6 +215,7 @@ class SPTFY:
 
 
 spot = SPTFY()
+spot.initialize_instance()
 
 
 def generate_random_string(length: int = None, min_lenght: int = 43, max_lenght: int = 128) -> str:
